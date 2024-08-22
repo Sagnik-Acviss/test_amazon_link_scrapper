83410f923e4bd8a181dddd411b530150b333a12c
Sagnik Chakraborty <130989352+Sagnik-Acviss@users.noreply.github.com>
2024-08-22 17:39:59 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -5,7 +5,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-@st.experimental_singleton
+@st.experimental_memo
 def get_driver():
     return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
@@ -14,6 +14,6 @@ options.add_argument('--disable-gpu')
 options.add_argument('--headless')
 
 driver = get_driver()
-driver.get('http://example.com')
+driver.get('http://amazon.com')
 
 st.code(driver.page_source)
